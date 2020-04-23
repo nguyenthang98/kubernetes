42b79c12e95af7e37162114a5fc59e7ee22cf731
Nguyen Duc Thang <nguyenducthang.uet@gmail.com>
2020-04-23 10:54:41 +0000

    fist commit

---
+++ vagrant-provisioning/README.md
@@ -0,0 +1,4 @@
+# provision kubernete cluster
+
+### Firstly, install vagrant, libvirt, qemu and vagrant-libvirt plugin
+### Secondly, vagrant up, wait and enjoy your cluster
---
+++ vagrant-provisioning/bootstrap-worker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+function log() {
+	echo -e "\e[93m$@\e[0m"
+}
+
+log "[TASK 1] Join node to kubernetes cluster"
+apt-get install -y sshpass >/dev/null 2>&1
+sshpass -p "kubeadmin" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no master-node:/joincluster.sh /joincluster.sh 2>/dev/null
+bash /joincluster.sh >/dev/null 2>&1
